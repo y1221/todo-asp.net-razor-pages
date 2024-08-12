f950807f91fe711e534ed901e513c855362a751a
okayu <yuto.gio.0515@gmail.com>
2024-08-12 18:26:24 +0900

    検証規則修正

--- TodoRazorApp/Models/Account.cs
+++ TodoRazorApp/Models/Account.cs
@@ -9,7 +9,6 @@ namespace TodoRazorApp.Models
         [Display(Name = "名前")]
         [DataType(DataType.Text)]
         [StringLength(30)]
-        [Required]
         public string Name { get; set; } = string.Empty;
 
         [Display(Name = "メールアドレス")]
