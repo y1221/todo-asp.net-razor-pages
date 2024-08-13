584a23ff98232f661a09c6b17256053a0295b605
okayu <yuto.gio.0515@gmail.com>
2024-08-13 15:03:33 +0900

    DBマイグレーション

--- TodoRazorApp/Migrations/20240813055642_AddFK.Designer.cs
+++ TodoRazorApp/Migrations/20240813055642_AddFK.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TodoRazorApp.Data;
 
@@ -11,9 +12,11 @@ using TodoRazorApp.Data;
 namespace TodoRazorApp.Migrations
 {
     [DbContext(typeof(TodoRazorAppContext))]
-    partial class TodoRazorAppContextModelSnapshot : ModelSnapshot
+    [Migration("20240813055642_AddFK")]
+    partial class AddFK
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TodoRazorApp/Migrations/20240813055642_AddFK.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoRazorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Todo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todo_AccountId",
+                table: "Todo",
+                column: "AccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todo_Account_AccountId",
+                table: "Todo",
+                column: "AccountId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todo_Account_AccountId",
+                table: "Todo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Todo_AccountId",
+                table: "Todo");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Todo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
