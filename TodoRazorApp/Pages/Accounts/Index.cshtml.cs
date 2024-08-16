da30a469668ed1247a22ad702594a2b689260be7
okayu <yuto.gio.0515@gmail.com>
2024-08-16 18:04:57 +0900

    ログイン時にはAccountモデルを使用しない（する必要がない）

--- TodoRazorApp/Pages/Accounts/Index.cshtml.cs
+++ TodoRazorApp/Pages/Accounts/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -28,7 +29,14 @@ namespace TodoRazorApp.Pages.Accounts
         }
 
         [BindProperty]
-        public Account Account { get; set; } = default!;
+        [Display(Name = "メールアドレス")]
+        [DataType(DataType.EmailAddress)]
+        public string? Mail { get; set; }
+
+        [BindProperty]
+        [Display(Name = "パスワード")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -38,23 +46,21 @@ namespace TodoRazorApp.Pages.Accounts
             }
 
             // 未入力チェック
-            if (string.IsNullOrEmpty(Account.Mail)
-                || string.IsNullOrEmpty(Account.Password))
+            if (string.IsNullOrEmpty(Mail) || string.IsNullOrEmpty(Password))
             {
                 ErrorMsg = "未入力項目があります";
                 return Page();
             }
 
             // ログインチェック
-            var account = await _context.Account.FirstOrDefaultAsync(m => m.Mail == Account.Mail && m.Password == Account.Password);
+            var account = await _context.Account.FirstOrDefaultAsync(m => m.Mail == Mail && m.Password == Password);
             if (account == null)
             {
                 ErrorMsg = "メールアドレスまたはパスワードが誤っています";
                 return Page();
             }
 
-            // ●セッションも利用してみる
-            HttpContext.Session.SetString("testSession", "abcde");
+            // セッションにAccount.Idを保存
             HttpContext.Session.SetInt32("accountId", account.Id);
 
             return RedirectToPage("../Todos/Index");
